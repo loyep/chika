cbb49a24545c2b5aa114bf0148220810faea28ec
Zack <lorenx@163.com>
2022-12-22 10:29:33 +0800

    feat: update posts

--- apps/web/src/pages/console/posts/index.tsx
+++ apps/web/src/pages/console/posts/index.tsx
@@ -1,8 +1,49 @@
+import { Table } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
 import { ReactElement } from 'react';
 import ConsoleLayout from '../../../layouts/console';
 
+interface DataType {
+  id: number;
+  title: string;
+}
+
+const columns: ColumnsType<DataType> = [
+  {
+    title: 'Title',
+    dataIndex: 'title',
+    key: 'title',
+    render: (text) => <a>{text}</a>,
+  },
+];
+
+// rowSelection object indicates the need for row selection
+const rowSelection = {
+  onChange: (selectedRowKeys: React.Key[], selectedRows: DataType[]) => {
+    console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+  },
+};
+
+const data: DataType[] = [
+  {
+    title: 'test',
+    id: 1,
+  },
+];
+
 export default function Posts() {
-  return <div>Posts</div>;
+  return (
+    <div>
+      <Table
+        rowSelection={{
+          type: 'checkbox',
+          ...rowSelection,
+        }}
+        columns={columns}
+        dataSource={data}
+      />
+    </div>
+  );
 }
 
 Posts.getLayout = function getLayout(page: ReactElement) {
